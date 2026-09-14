406c8d1698b8c635f554af25143fcf38619c80b2
sabbirislamshanto9-cell <sabbirislamshanto9@gmail.com>
2026-09-14 13:12:08 +0600

    Change default credentials in env_patch.py
    Updated default credentials in env_patch.py documentation.

--- env_patch.py
+++ env_patch.py
@@ -1,6 +1,6 @@
 """env_patch.py - Railway environment ভ্যারিয়েবল দিয়ে panel login override করার মিনিমাল প্যাচ।
 Railway এ Variables ট্যাবে PANEL_SECRET ও PANEL_USER_PASS বসালেই লগিন পাসওয়ার্ড বদলে যায়।
-Default (FSFUHX / FXFUHXFFKING) আগের মতোই কাজ করে।
+Default (ASOWNER99 / ARYANSHANTO99) আগের মতোই কাজ করে।
 ব্যবহার: app.py এ `import env_patch` যোগ করুন (import ব্লকের নিচে)।
 """
 import os
